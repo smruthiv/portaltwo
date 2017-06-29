4bc5051ca9d7a12190a7dc54f9f375926867657a
ssiadmin <ssiadmin99>
2017-06-29 13:30:17 +0530

    Added users docs.

---
+++ modules/UsersDocs/src/main/java/usersdocs/portlet/EmployeeDirectoryConfiguration.java
@@ -0,0 +1,35 @@
+package usersdocs.portlet;
+
+import com.liferay.portal.kernel.portlet.ConfigurationAction;
+import com.liferay.portal.kernel.portlet.DefaultConfigurationAction;
+import com.liferay.portal.kernel.util.ParamUtil;
+
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
+import javax.portlet.PortletConfig;
+import javax.portlet.PortletPreferences;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.ConfigurationPolicy;
+
+@Component(
+	    configurationPid = "usersdocs.portlet.EmployeeDirectoryConfiguration",
+		    configurationPolicy = ConfigurationPolicy.OPTIONAL,
+		    immediate = true,
+		    property = {
+		        "javax.portlet.name=usersdocs_portlet_UsersDocsPortlet"
+		    },
+		    service = ConfigurationAction.class
+		)
+public class EmployeeDirectoryConfiguration extends DefaultConfigurationAction{
+	@Override
+	public void processAction(PortletConfig portletConfig, ActionRequest actionRequest, ActionResponse actionResponse)
+			throws Exception {
+		String userGroupId =ParamUtil.getString(actionRequest,"userGroup");
+		PortletPreferences portletPreferences = actionRequest.getPreferences();
+		portletPreferences.setValue("userGroupId", userGroupId);
+		portletPreferences.store();
+		super.processAction(portletConfig, actionRequest, actionResponse);
+			
+	}
+}
